5b76a3b57c0e77071623dab7f0a20afc406bb3e2
umnikos <alex.stefanov2@gmail.com>
2020-12-27 17:39:03 +0200

    Add global descriptor table

--- boot.asm
+++ boot.asm
@@ -12,6 +12,36 @@ align 16
 stack_bottom:   resb 16384
 stack_top:
 
+section .data
+; references: https://wiki.osdev.org/GDT_Tutorial and https://wiki.osdev.org/Global_descriptor_table
+gdtr:           dw gdt_end-gdt-1
+                dd gdt
+gdt:
+                ; null segment (0x00) (has to be empty)
+                dw 0                            ; limit 0-15
+                dw 0                            ; base 0-15
+                db 0                            ; base 16-23
+                db 0                            ; type
+                db 0                            ; limit 16-19 | flags
+                db 0                            ; base 24-31
+
+                ; code segment (0x08)
+                dw 0xFFFF
+                dw 0
+                db 0
+                db 0x9A
+                db 0xCF
+                db 0
+
+                ; data segment (0x10)
+                dw 0xFFFF
+                dw 0
+                db 0
+                db 0x92
+                db 0xCF
+                db 0
+gdt_end:
+
 section .multiboot
 align 4
                 dd MAGIC_NUMBER                 ; write the magic number to the machine code,
@@ -19,10 +49,28 @@ align 4
                 dd CHECKSUM                     ; and the checksum
 
 section .text
+init_gdt:
+                lgdt [gdtr]                     ; load global descriptor table
+                jmp 0x08:.reload_cs             ; a far-jump to change cs segment register
+.reload_cs:
+                mov ax, 0x10                    ; change every other segment register
+                mov ds, ax
+                mov es, ax
+                mov fs, ax
+                mov gs, ax
+                mov ss, ax
+                ret
+
 global _start
 extern kernel_main
 _start:
-                mov esp, stack_top
+                cli                             ; disable interrupts while we're setting up stuff
+
+                mov esp, stack_top              ; setup the stack (needed for C)
+
+                call init_gdt
+
+                sti                             ; enable interrupts again
                 call kernel_main
 global halt
 halt:           cli
