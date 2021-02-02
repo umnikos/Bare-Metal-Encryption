bdb1b2a72e82a27934396fab09527e40099f5fd6
umnikos <alex.stefanov2@gmail.com>
2021-02-02 18:39:51 +0200

    refactored gimme_memory

--- kernel/heap.c
+++ kernel/heap.c
@@ -1,19 +1,23 @@
 #include "prelude.h"
 
-// not actually functions!
-extern void heap_size();
-extern void heap_start();
-extern void heap_end();
+#define heap_size 32
+static u8 heap_start[heap_size*4096] __attribute__((aligned(4096)));
 
 u8* gimme_memory(u32 pages) {
   static u32 given_pages = 0;
+  if ((u32)heap_size != 32) {
+    debug("WRONG HEAP SIZE!\n");
+    halt();
+  }
   if ((u32)heap_start & 0x00000FFF) { // this check isn't necessary, it's here for my own sanity
+    debug("MISALIGNED HEAP\n");
     crash(0xBAADF00D);
   }
   if (pages + given_pages > (u32)heap_size) {
+    debug("HEAP OVERFLOW\n");
     crash(0x8BADF00D);
   }
-  u8* res = (u8*)((u32)heap_start + (given_pages<<12));
+  u8* res = heap_start + (given_pages<<12);
   given_pages += pages;
   return res;
 }
